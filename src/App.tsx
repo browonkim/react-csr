c5effc1f393f7826c3aceef0f93a2b7b867f40d8
browonkim <browonkim@gmail.com>
2022-05-09 01:08:32 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import { TodoView } from './views/TodoView'
 import { ListView } from './views/ListView'
 import { HomeView } from './views/HomeView'
+import { CommonHeader } from './layout/CommonHeader'
 
 function App () {
   return (
@@ -11,7 +12,14 @@ function App () {
             <Routes>
                 <Route path="/" element={<TodoView/>}/>
                 <Route path="/list" element={<ListView/>}/>
-                <Route path="/home" element={<HomeView/>}/>
+                <Route path="/home" element={
+                    <main>
+                        <CommonHeader/>
+                        <HomeView/>
+                    </main>
+                }>
+                    <Route path="" element={<HomeView/>}/>
+                </Route>
             </Routes>
         </div>
   )
---
+++ src/layout/CommonHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function CommonHeader () {
+  return (
+        <header className="CommonHeader">
+            <h1>Browon</h1>
+        </header>
+  )
+}
